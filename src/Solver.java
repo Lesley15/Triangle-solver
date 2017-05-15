216202ec207dd03d1e38aefa016ef9ea2da3072f
Lesley15 <lesley.isaro@ocsbstudent.ca>
2017-05-15 13:22:12 -0400

    .

---
+++ src/Solver.java
@@ -0,0 +1,62 @@
+import trig.Trig;
+import java.util.*;
+public class Solver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Trig triangle=new Trig();
+		Scanner in=new Scanner(System.in);
+		   System.out.println("Hello, welcome to the Trigonometry calculator");
+	       System.out.println("You must enter a minimum of three measurements. \nOne of the measurements must be a length/side.");
+	  
+	       System.out.println(" Please enter side length a:");
+	       double a= in.nextDouble();
+	       System.out.println("Please enter side length b:");
+	       double b= in.nextDouble();
+	       System.out.println(" Please enter side length c:");
+	       double c= in.nextDouble();
+	       System.out.println(" Please enter angle A:");
+	       double A= in.nextDouble();
+	       System.out.println(" Please enter angle B:");
+	       double B= in.nextDouble();
+	       System.out.println(" Please enter angle C:");
+	       double C= in.nextDouble();
+	       //year=Integer.parseInt(words[0]);
+	       //double[] values={a,b,c,A,B,C};
+	       //Working on commands blueprint first, before coding
+	       //check when only three sides
+	       if(A==0 & B==0 && C==0){
+	    	   
+	       }
+	       //Three pieces of info missing, therefore 3 given
+	       if(b==0 && c==0 && A==0){
+	    	   A= threeAngles(B, C);
+	    	   b= triangle.sinLawSide(a, A, B);
+	    	   c= triangle.sinLawSide(b, B, C);
+	       }
+	       if (B==0 && a==0 && c==0){
+	    	   B=threeAngles(A, C);
+	    	   a=triangle.sinLawSide(b, B, A);
+	    	   c=triangle.sinLawSide(a, A, C);
+	       }
+	       //Now check for if four pieces of info are given
+	       if (B==0 && C==0){
+	    	   B=triangle.sinLawngle(a, A, b);
+	    	   C=triangle.sinLawngle(b, B, c);
+	       }
+	       if(A==0 && c==0){
+	    	   A=threeAngles(B, C);
+	    	   c=triangle.sinLawSide(a, A, C);
+	       }
+	       if(a==0 && b==0){
+	    	   a=triangle.sinLawSide(c, C, A);
+	    	   b=triangle.sinLawSide(a, A, B);
+	       }
+	      System.out.println("Therefore, a= "+a+" b= "+b+" c= "+c+" A= "+A+" B= "+B+" C= "+C );
+		}
+		public static double threeAngles (double A, double B){
+			double C= 180-(A + B);
+			return C;
+	}
+
+}
